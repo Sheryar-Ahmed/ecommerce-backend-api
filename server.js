e81bd0a4f78c01c2b596970aeefb1f60b325749a
royalsheryar505@gmail.com <royalsheryar505@gmail.com>
2023-06-15 13:34:13 +0500

    setup railway Closed # 166

--- server.js
+++ server.js
@@ -6,11 +6,9 @@ const cloudinary = require('cloudinary');
 dbConn(); //mongodb connection
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINAR_API_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET_KEY,
-  });
-  
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINAR_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
+});
 
-
-app.listen(PORT, () => console.log(`server is running on ${PORT}`))
+app.listen(PORT, "0.0.0.0", () => console.log(`server is running on ${PORT}`))
