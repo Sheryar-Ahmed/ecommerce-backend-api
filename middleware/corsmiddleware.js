e08a70422e14eed44f1c2cf68b26fb93f978f94b
royalsheryar505@gmail.com <royalsheryar505@gmail.com>
2023-06-15 13:58:35 +0500

    trying to resolve cors error

--- middleware/corsmiddleware.js
+++ middleware/corsmiddleware.js
@@ -1,8 +1,8 @@
 // Custom middleware to set Access-Control-Allow-Origin dynamically based on the request origin
 function setCorsHeaders(handler) {
     return function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-        res.setHeader('Access-Control-Allow-Credentials', 'true');
+        res.setHeader('Access-Control-Allow-Origin', "*");
+        res.setHeader('Access-Control-Allow-Credentials', true);
         handler(req, res, next);
     };
 }
