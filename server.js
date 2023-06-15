fd1cf88e54d1ebe7eb8a330c28b23c5d7ce3db59
royalsheryar505@gmail.com <royalsheryar505@gmail.com>
2023-06-15 13:37:34 +0500

    Revert "setup railway Closed # 166"
    This reverts commit e81bd0a4f78c01c2b596970aeefb1f60b325749a.

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const cloudinary = require('cloudinary');
 dbConn(); //mongodb connection
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINAR_API_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET_KEY,
-});
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINAR_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET_KEY,
+  });
+  
 
-app.listen(PORT, "0.0.0.0", () => console.log(`server is running on ${PORT}`))
+
+app.listen(PORT, () => console.log(`server is running on ${PORT}`))
