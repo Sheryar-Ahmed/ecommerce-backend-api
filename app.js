60cecec89413459ffb049d5b0b7d78bbfd1b37e6
royalsheryar505@gmail.com <royalsheryar505@gmail.com>
2023-06-15 13:18:17 +0500

    add origin url to evn file Closed # 165

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const app = express();
 
-app.use(cors({ origin: "http://192.168.43.72:3000", credentials: true }));
+app.use(cors({ origin: `${process.env.REACT_APP_BASE_URL}`, credentials: true }));
 app.use(express.json({ limit: '25mb' }));
 app.use(express.urlencoded({ limit: '25mb', extended: true }));
 app.use(cookieParser());
